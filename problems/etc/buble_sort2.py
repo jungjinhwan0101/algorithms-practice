30bc7be7f37930454e7b3ac3391d36d94387a7d5
JungJinHwan <jungjinhwan0101@gmail.com>
2025-02-13 11:50:41 +0900

    버블 정렬 복습

--- problems/etc/buble_sort2.py
+++ problems/etc/buble_sort2.py
@@ -1,9 +1,9 @@
 def bubble_sort(arr: list[int]) -> list[int]:
     n = len(arr)
 
-    for i in range(n):
+    for i in range(1, n):
         swapped = False
-        for j in range(0, n - i - 1):
+        for j in range(n - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
